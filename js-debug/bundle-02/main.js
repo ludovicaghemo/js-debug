d1e61eebeca17286092c6d75cd3f7ff49f0c3e2b
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-10 16:08:31 +0100

    add: boundle 02

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -23,12 +23,30 @@ function checkAge() {
 }
 checkAge();
 
+// 1- Che cosa fa questo codice?
+// Questo codice definisce una funzione checkAge.
+
+// 2- Sono presenti errori di sintassi?
+// No
+
+// 3- Sono presenti errori logici?
+// Sì, la funzione non restituisce o stampa alcun risultato perchè non c'è return
+
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
 }
 printColorsNumber();
+
+// 1- Che cosa fa questo codice?
+// Questo codice dovrebbe stampare il numero di colori nella palette
+
+// 2- Sono presenti errori di sintassi?
+// Sì --> c'è un errore di battitura. Dovrebbe essere colors.length (invece di colors.lenght).
+
+// 3- Sono presenti errori logici?
+// No
 
 
 // ESERCIZIO 3
@@ -39,6 +57,15 @@ function addNumbers() {
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+
+// 1- Che cosa fa questo codice?
+// Questo codice chiede all'utente di inserire un numero, somma 12 e stampa il risultato.
+
+// 2- Sono presenti errori di sintassi?
+// No
+
+// 3- Sono presenti errori logici?
+// Sì, c'è un errore logico perché userNumber è una stringa --> dovrebbe essere convertito in numero tramite parseInt (per esempio). 
 
 
 // ESERCIZIO 4
@@ -59,6 +86,16 @@ function checkAccess() {
     }
 }
 checkAccess();
+
+// 1- Che cosa fa questo codice?
+// Questo codice chiede all'utente di inserire un indirizzo email; verifica se l'indirizzo inserito è presente nell'array addresses, e poi stampa un messaggio di accesso consentito o accesso negato.
+
+// 2- Sono presenti errori di sintassi?
+// No.
+
+// 3- Sono presenti errori logici?
+// No
+
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -91,29 +128,11 @@ function checkAccessImproved() {
 
 
 
+// 1- Che cosa fa questo codice?
+// Questo codice chiede all'utente di inserire un indirizzo email, verifica se la lunghezza è maggiore di 5 e se corrisponde a uno degli indirizzi email nell'array addresses, e poi un messaggio di accesso consentito o negato.
 
+// 2- Sono presenti errori di sintassi?
+// La funzione non ha la parentesi graffa di chiusura
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 3- Sono presenti errori logici?
+// No
